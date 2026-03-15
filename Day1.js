3b1cdb14cae0bd3c576fb09fc31a9152fb478b1c
Pankaj <pankajtakmoge2110@gmail.com>
2026-03-15 13:11:22 +0530

    update

--- Day1.js
+++ Day1.js
@@ -1,10 +1,4 @@
-function greet(name) {
-  console.log("Hello " + name);
-}
-
-function processUser(callback) {
-  let name = "Pankaj";
-  callback(name); // calling the passed function
-}
-
-processUser(greet); // "Hello Pankaj"
+let add = function (a,b) {
+    console.log(a-b);
+};
+add (10,50)
