240daf933392d54ae015970f055156f7c298e778
Pankaj <pankajtakmoge2110@gmail.com>
2026-03-15 18:03:23 +0530

    Added Project

--- Day1.js
+++ Day1.js
@@ -1,4 +1,25 @@
-let add = function (a,b) {
-    console.log(a-b);
-};
-add (10,50)
+console.log(calculateArea(10, 10, triangle))
+// Here we used hoisting to print area of triangle and run calculateArea func before its declaration
+
+function calculateArea (a, b, areafunction) {
+    return areafunction(a,b);
+}
+// area of Reactangle
+const reactangle = (l,b) => l * b;
+
+// area of square
+const square = (side) => side * side;
+
+//area of circle
+const circle = (r) => Math.PI * r * r ;
+
+//area of Triangle
+function triangle (b, h) {
+    return 0.5 * b * h;
+}
+
+console.log(calculateArea(10, 10, circle))
+console.log(calculateArea(10, 10, reactangle))
+console.log(calculateArea(10, 10, square))
+
+
