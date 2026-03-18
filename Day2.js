384fa2e30d35b44f407c4828df02d3461ddad6e4
Pankaj <pankajtakmoge2110@gmail.com>
2026-03-18 21:03:29 +0530

    added project

--- Day2.js
+++ Day2.js
@@ -1,4 +1,23 @@
-let a = ["a","b","c","d"];
+let arr = [10, 12, 5, 56, 63, 26, 3, 75, 89, 1];
 
-let bcc =a.reverse();
-console.log(bcc);
+let sum =0;
+
+let max =arr[0];
+let min =arr[0];
+for (let i =0; i<arr.length; i++) {
+ sum+=arr[i];
+
+ if(arr[i]>max){
+    max =arr[i];
+ }
+ if(arr[i]<min){
+    min = arr[i];
+ }
+}
+ let average =sum/arr.length;
+
+console.log(sum);
+console.log(max);
+console.log(min);
+console.log(average);
+
