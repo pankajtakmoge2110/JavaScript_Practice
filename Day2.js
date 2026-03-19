b0df188db18faf4fa328a9fd5eb74bedd8a76b50
Pankaj <pankajtakmoge2110@gmail.com>
2026-03-19 18:36:02 +0530

    updated

--- Day2.js
+++ Day2.js
@@ -4,16 +4,7 @@ let sum =0;
 
 let max =arr[0];
 let min =arr[0];
-for (let i =0; i<arr.length; i++) {
- sum+=arr[i];
 
- if(arr[i]>max){
-    max =arr[i];
- }
- if(arr[i]<min){
-    min = arr[i];
- }
-}
 
 
 
