315bf8fd5a44c93c40c92a51acedf0c3eb934e82
Pankaj <pankajtakmoge2110@gmail.com>
2026-03-28 20:36:02 +0530

    updated

--- Day2.js
+++ Day2.js
@@ -3,9 +3,7 @@ let abc =["a","b","c","d"];
 for(let i = 0; i<abc.length; i++){
   if ("a" === abc[i]){
     console.log("it is there");
-  }else{
-    console.log("it is not there");    
-  };
+  }
 };
 
 
